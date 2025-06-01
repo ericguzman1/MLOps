7b9ead10c619cc74f21aa5e7cf56adcb8fb53e29
ericguzman1 <119265733+ericguzman1@users.noreply.github.com>
2025-06-01 12:59:55 -0400

    Update MLOps.py

--- scripts/MLOps.py
+++ scripts/MLOps.py
@@ -17,9 +17,9 @@ ml_client = MLClient(
 # Define the base directory
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
-step_process = load_component(source=os.path.join(base_dir, "../components/step_process.yml"))
+step_process = load_component(source=os.path.join(base_dir, "../components/data_prep.yml"))
 train_step = load_component(source=os.path.join(base_dir, "../components/train_step.yml"))
-model_register_component = load_component(source=os.path.join(base_dir, "../components/model_register_component.yml"))
+model_register_component = load_component(source=os.path.join(base_dir, "../components/model_register.yml"))
 
 # Define pipeline
 @pipeline(compute="cpu-cluster", description="Pipeline for data preparation, training, and model registration")
